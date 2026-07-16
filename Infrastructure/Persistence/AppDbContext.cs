445fe6a3096d724a54d8fc38ff77d13612bd7bd0
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2026-07-16 16:44:29 +0300

    Add OnModelCreating

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -14,4 +14,14 @@ public class AppDbContext : DbContext
     public DbSet<TicketPriority> TicketPriorities { get; set; }
     public DbSet<Ticket> Tickets { get; set; }
     public DbSet<TicketAudit> TicketAudits { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<Ticket>().ToTable("Ticket");
+        modelBuilder.Entity<TicketStatus>().ToTable("TicketStatus");
+        modelBuilder.Entity<TicketPriority>().ToTable("TicketPriority");
+        modelBuilder.Entity<TicketAudit>().ToTable("TicketAudit");
+    }
 }
